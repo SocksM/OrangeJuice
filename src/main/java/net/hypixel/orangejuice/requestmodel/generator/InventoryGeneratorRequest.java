5aabe24f6c01746b8cc0fbe0fe59c9eedebeb95d
SocksM <67207504+SocksM@users.noreply.github.com>
2025-04-16 16:19:17 +0200

    Small documentation bugfix

--- src/main/java/net/hypixel/orangejuice/requestmodel/generator/InventoryGeneratorRequest.java
+++ src/main/java/net/hypixel/orangejuice/requestmodel/generator/InventoryGeneratorRequest.java
@@ -9,21 +9,21 @@ import lombok.EqualsAndHashCode;
 @Data
 public class InventoryGeneratorRequest extends GeneratorRequestModelBase {
 
-    @Schema(description = "The inventory string to be used for the generation", example = "") // TODO: example
-    private String InventoryString;
+    @Schema(description = "The inventory string to be used for the generation", example = "TODO") // TODO: example
+    private String inventoryString;
 
     @Schema(description = "The number of rows in the inventory", example = "6")
-    private int Rows;
+    private int rows;
 
     @Schema(description = "The number of slots per row in the inventory", example = "9")
-    private int SlotsPerRow;
+    private int slotsPerRow;
 
-    @Schema(description = "The hovered item string to be used for the generation", example = "") // TODO: example
-    private String HoveredItemString;
+    @Schema(description = "The hovered item string to be used for the generation", example = "TODO") // TODO: example
+    private String hoveredItemString;
 
     @Schema(description = "The name of the container", example = "My Inventory")
-    private String ContainerName;
+    private String containerName;
 
     @Schema(description = RENDER_BORDER_DESCRIPTION, example = RENDER_BORDER_EXAMPLE)
-    private boolean RenderBorder;
+    private boolean renderBorder;
 }
