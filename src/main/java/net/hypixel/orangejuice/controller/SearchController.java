4ecb8395cc0457fa071675ec2b46d47756a18e98
SocksM <67207504+SocksM@users.noreply.github.com>
2025-04-20 01:22:54 +0200

    api/item-id -> api/item-id

--- src/main/java/net/hypixel/orangejuice/controller/SearchController.java
+++ src/main/java/net/hypixel/orangejuice/controller/SearchController.java
@@ -15,12 +15,12 @@ import java.util.List;
 @RequestMapping("/search")
 public class SearchController {
 
-    @GetMapping("/itemid")
+    @GetMapping("/item-id")
     public ResponseEntity itemIds() {
         return ResponseEntity.ok(AutoCompleteService.itemNamesAutoCompletes());
     }
 
-    @GetMapping("/itemid/{searchTerm}")
+    @GetMapping("/item-id/{searchTerm}")
     public ResponseEntity itemIds(@PathVariable String searchTerm) {
         return ResponseEntity.ok(contains(AutoCompleteService.itemNamesAutoCompletes(), searchTerm));
     }
