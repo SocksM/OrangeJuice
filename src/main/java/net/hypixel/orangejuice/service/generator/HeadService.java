62d97a3a44d473d610b1d0aed281bcdb83933f33
SocksM <67207504+SocksM@users.noreply.github.com>
2025-04-20 00:40:51 +0200

    Oops (im good at git trust) pt2

--- src/main/java/net/hypixel/orangejuice/service/generator/HeadService.java
+++ src/main/java/net/hypixel/orangejuice/service/generator/HeadService.java
@@ -3,15 +3,10 @@ package net.hypixel.orangejuice.service.generator;
 import net.hypixel.orangejuice.generator.image.GeneratorImageBuilder;
 import net.hypixel.orangejuice.generator.impl.MinecraftPlayerHeadGenerator;
 import net.hypixel.orangejuice.generator.item.GeneratedObject;
-import net.hypixel.orangejuice.requestmodel.generator.HeadGeneratorRequest;
 
 public class HeadService {
 
     public static GeneratedObject generate(String skinValue) {
-        if (skinValue == null) {
-            skinValue = DEFAULT_SKIN_VALUE;
-        }
-
         return new GeneratorImageBuilder()
             .addGenerator(new MinecraftPlayerHeadGenerator.Builder().withSkin(skinValue).build())
             .build();
